ca997ca71dee1cad4b4a0128cd34edb37c661489
Mysticpasta1 <ethan.cantrell@asub.edu>
2022-07-06 11:39:48 -0500

    fix

--- src/main/java/net/montoyo/mcef/mixins/ForgeInitEquavilentMixin.java
+++
@@ -1,8 +0,0 @@
-package net.montoyo.mcef.mixins;
-
-import net.minecraft.client.Minecraft;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(Minecraft.class)
-public class ForgeInitEquavilentMixin {
-}
