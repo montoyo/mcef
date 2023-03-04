11327ce1872a1295d20b66c3e41e9a56056e25e7
GiantLuigi4 <49770992+GiantLuigi4@users.noreply.github.com>
2023-03-04 18:28:34 -0500

    various fixes to keyboard handling, scrolling, and mouse motion

--- src/main/java/net/montoyo/mcef/client/UnsafeExample.java
+++ src/main/java/net/montoyo/mcef/client/UnsafeExample.java
@@ -1,5 +1,6 @@
 package net.montoyo.mcef.client;
 
+import org.cef.OS;
 import sun.misc.Unsafe;
 
 import java.awt.*;
@@ -13,6 +14,8 @@ public class UnsafeExample {
     private static final Field keyCode;
     private static final Field keyChar;
     private static final Field keyLocation;
+    private static final Field keyScanCode;
+    private static final Field keyRawCode;
     private static final Field id;
     private static final Field when;
     private static final Field modifs;
@@ -20,6 +23,8 @@ public class UnsafeExample {
     private static final long offsetCode;
     private static final long offsetChar;
     private static final long offsetLocation;
+    private static final long offsetScanCode;
+    private static final long offsetRawCode;
     private static final long offsetId;
     private static final long offsetWhen;
     private static final long offsetModifs;
@@ -32,6 +37,7 @@ public class UnsafeExample {
             keyCode = KeyEvent.class.getDeclaredField("keyCode");
             keyChar = KeyEvent.class.getDeclaredField("keyChar");
             keyLocation = KeyEvent.class.getDeclaredField("keyLocation");
+            keyRawCode = KeyEvent.class.getDeclaredField("rawCode");
             id = AWTEvent.class.getDeclaredField("id");
             when = InputEvent.class.getDeclaredField("when");
             modifs = InputEvent.class.getDeclaredField("modifiers");
@@ -39,14 +45,26 @@ public class UnsafeExample {
             offsetCode = theUnsafe.objectFieldOffset(keyCode);
             offsetChar = theUnsafe.objectFieldOffset(keyChar);
             offsetLocation = theUnsafe.objectFieldOffset(keyLocation);
+            offsetRawCode = theUnsafe.objectFieldOffset(keyRawCode);
             offsetId = theUnsafe.objectFieldOffset(id);
             offsetWhen = theUnsafe.objectFieldOffset(when);
             offsetModifs = theUnsafe.objectFieldOffset(modifs);
+
+            if (OS.isWindows()) {
+                keyScanCode = KeyEvent.class.getDeclaredField("scancode");
+                offsetScanCode = theUnsafe.objectFieldOffset(keyScanCode);
+            } else {
+                keyScanCode = null;
+                offsetScanCode = 0;
+            }
         } catch (Throwable e) {
             throw new RuntimeException("Unable to get theUnsafe.");
         }
     }
-    public KeyEvent makeEvent(Component src, int keyCode, char keyChar, int location, int type, long time, int modifs) {
+    public static KeyEvent makeEvent(Component src, int keyCode, char keyChar, int location, int type, long time, int modifs, long scanCode) {
+        return makeEvent(src, keyCode, keyChar, location, type, time, modifs, scanCode, keyCode);
+    }
+    public static KeyEvent makeEvent(Component src, int keyCode, char keyChar, int location, int type, long time, int modifs, long scanCode, long raw) {
         KeyEvent event = new KeyEvent(src, KeyEvent.KEY_PRESSED, 0, 0, KeyEvent.VK_E, 'e', KeyEvent.KEY_LOCATION_STANDARD);
         theUnsafe.putInt(event, offsetCode, keyCode);
         theUnsafe.putInt(event, offsetChar, keyChar);
@@ -54,6 +72,11 @@ public class UnsafeExample {
         theUnsafe.putInt(event, offsetId, type);
         theUnsafe.putInt(event, offsetModifs, modifs);
         theUnsafe.putLong(event, offsetWhen, time);
+        theUnsafe.putLong(event, offsetRawCode, raw);
+        
+        if (offsetScanCode != 0) {
+            theUnsafe.putLong(event, offsetScanCode, scanCode);
+        }
         return event;
     }
 }
