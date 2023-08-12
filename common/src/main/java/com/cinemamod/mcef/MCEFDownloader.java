841441f72a03b31d4a0dba0dce6afbea061fdd70
ds58 <30220598+ds58@users.noreply.github.com>
2023-08-12 16:24:25 -0500

    Start downloader

---
+++ common/src/main/java/com/cinemamod/mcef/MCEFDownloader.java
@@ -0,0 +1,67 @@
+package com.cinemamod.mcef;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.function.Consumer;
+
+public class MCEFDownloader {
+    private static final String JAVA_CEF_DOWNLOAD_URL = "https://mcef-download.cinemamod.com/java-cef-builds/${java-cef-commit}/${platform}.tar.gz";
+    private static final String JAVA_CEF_CHECKSUM_DOWNLOAD_URL = "https://mcef-download.cinemamod.com/java-cef-builds/${java-cef-commit}/${platform}.tar.gz.sha256";
+
+    private final String javaCefCommitHash;
+    private final MCEFPlatform platform;
+
+    public MCEFDownloader(String javaCefCommitHash, MCEFPlatform platform) {
+        this.javaCefCommitHash = javaCefCommitHash;
+        this.platform = platform;
+    }
+
+    public String getJavaCefDownloadUrl() {
+        return formatURL(JAVA_CEF_DOWNLOAD_URL);
+    }
+
+    public String getJavaCefChecksumDownloadUrl() {
+        return formatURL(JAVA_CEF_CHECKSUM_DOWNLOAD_URL);
+    }
+
+    private String formatURL(String url) {
+        return url
+                .replace("${java-cef-commit}", javaCefCommitHash)
+                .replace("${platform}", platform.getNormalizedName());
+    }
+
+    public void downloadJavaCefBuild() throws IOException {
+        downloadFile(getJavaCefDownloadUrl(), new File(System.getProperty("mcef.libraries.path"), platform.getNormalizedName() + ".tar.gz"), System.out::println);
+    }
+
+    public void downloadJavaCefChecksum() throws IOException {
+        downloadFile(getJavaCefChecksumDownloadUrl(), new File(System.getProperty("mcef.libraries.path"), platform.getNormalizedName() + ".tar.gz.sha256"), System.out::println);
+    }
+
+    private static void downloadFile(String urlString, File outputFile, Consumer<Float> percentCompleteConsumer) throws IOException {
+        URL url = new URL(urlString);
+        URLConnection urlConnection = url.openConnection();
+        int fileSize = urlConnection.getContentLength();
+
+        BufferedInputStream inputStream = new BufferedInputStream(url.openStream());
+        FileOutputStream outputStream = new FileOutputStream(outputFile);
+
+        byte[] buffer = new byte[1024];
+        int count;
+        int readBytes = 0;
+        while((count = inputStream.read(buffer,0,1024)) != -1)
+        {
+            outputStream.write(buffer, 0, count);
+            readBytes += count;
+            float percentComplete = (float) readBytes / fileSize;
+            percentCompleteConsumer.accept(percentComplete);
+        }
+
+        inputStream.close();
+        outputStream.close();
+    }
+}
--- common/src/main/java/com/cinemamod/mcef/mixins/CefInitMixin.java
+++ common/src/main/java/com/cinemamod/mcef/mixins/CefInitMixin.java
@@ -2,7 +2,6 @@ package com.cinemamod.mcef.mixins;
 
 import com.cinemamod.mcef.MCEF;
 import com.cinemamod.mcef.MCEFClient;
-import com.cinemamod.mcef.MCEFPlatform;
 import net.minecraft.client.resources.ClientPackSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,52 +10,33 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.attribute.PosixFilePermission;
-import java.util.HashSet;
-import java.util.Set;
 
 @Mixin(ClientPackSource.class)
 public class CefInitMixin {
-    private static void setUnixExecutable(File file) {
-        Set<PosixFilePermission> perms = new HashSet<>();
-        perms.add(PosixFilePermission.OWNER_READ);
-        perms.add(PosixFilePermission.OWNER_WRITE);
-        perms.add(PosixFilePermission.OWNER_EXECUTE);
+    private static void setupLibraryPath() throws IOException {
+        final File mcefLibrariesDir;
 
-        try {
-            Files.setPosixFilePermissions(file.toPath(), perms);
-        } catch (IOException e) {
-            // Ignore
-        }
-    }
-
-    private static void setupLibraryPath(MCEFPlatform platform) throws IOException, URISyntaxException {
         // Check for development environment
-        // i.e. mcef-repo/forge/build/cef/<platform>
-        File cefPlatformDir = new File("../build/cef/" + platform.getNormalizedName());
-        if (cefPlatformDir.exists()) {
-            System.setProperty("jcef.path", cefPlatformDir.getCanonicalPath());
-            return;
+        // TODO: handle eclipse/others
+        // i.e. mcef-repo/forge/build
+        File buildDir = new File("../build");
+        if (buildDir.exists() && buildDir.isDirectory()) {
+            mcefLibrariesDir = new File(buildDir, "mcef-libraries/");
+        } else {
+            mcefLibrariesDir = new File("mods/mcef-libraries/");
         }
 
-        // Check for .minecraft/mods/mcef-libraries directory, create if not exists
-        File mcefLibrariesDir = new File("mods/mcef-libraries");
-        if (!mcefLibrariesDir.exists()) {
-            mcefLibrariesDir.mkdirs();
-        }
-        System.setProperty("jcef.path", mcefLibrariesDir.getCanonicalPath());
+        mcefLibrariesDir.mkdirs();
+
+        System.setProperty("mcef.libraries.path", mcefLibrariesDir.getCanonicalPath());
     }
 
     @Inject(at = @At("TAIL"), method = "<init>(Ljava/nio/file/Path;)V")
     private void init(CallbackInfo callbackInfo) {
-        MCEFPlatform platform = MCEFPlatform.getPlatform();
-
         try {
-            setupLibraryPath(platform);
-        } catch (IOException | URISyntaxException e) {
-            throw new RuntimeException(e);
+            setupLibraryPath();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
         MCEFClient client = MCEF.getClient();
